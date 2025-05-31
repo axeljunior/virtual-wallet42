ab6e27746766e3f651976789fe1a39134d003bf2
Axel Junior <axeljunior.dev@gmail.com>
2025-05-30 22:55:48 -0300

    ajuste no path das pastas em src

--- src/app.modules.ts
+++ src/app.modules.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './config/modules/users/users.module';
-import { TransactionsModule } from './config/modules/transactions/transactions.module';
+import { UsersModule } from './modules/users/users.module';
+import { TransactionsModule } from './modules/transactions/transactions.module';
 
 @Module({
   imports: [
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from '../src/config/modules/users/users.module';
+import { AppModule } from '../src/modules/users/users.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
